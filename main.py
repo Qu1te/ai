692d996242798a1762efe2590cb3fd2ce8de14b8
Qu1te <148548462+Qu1te@users.noreply.github.com>
2023-12-01 18:16:16 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
-"""import pygame
-import random
-
-
-pygame.init()
-
-screen = pygame.display.set_mode((800,600))
-
-pygame.display.set_caption("Rock, Paper, Scissors")
-icon = pygame.image.load("logo.png")
-pygame.display.set_icon(icon)
-clock = pygame.time.Clock()
-
-choices = ["rock", "paper", "scissors"]
-player_choice = None
-computer_choice = None
-
-running = True
-
-while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_r:
-            player_choice = "rock"
-	
-
-
-	screen.fill((217,197,161))
-	pygame.display.update()"""
-
 from tkinter import * 
 import random
+from random import choice
 
 window = Tk()
 window.geometry("800x600")
 window.title("rock paper scissors")
 
-frame = Frame(window,bg="lightblue")
+frame = Frame(window)
 frame.place(relx = 0.01,rely=0.01,relwidth=0.98,relheight=0.98)
 
 name = Label(frame,text="rock paper scissors - player vs computer", font=100)
@@ -50,7 +19,44 @@ label2 = Label(frame,text="vs",font="normal 15")
 label3 = Label(frame,text="computer",font="normal 15")
 
 label1.place(x=80,y=50,width=100)
-label1.place(x=80,y=50,width=100)
-label1.place(x=80,y=50,width=100)
+label2.place(x=350,y=50,width=100)
+label3.place(x=600,y=50,width=100)
+
+rock_png = PhotoImage(file = "rock.png")
+scissors_png = PhotoImage(file = "scissors.png")
+paper_png = PhotoImage(file = "paper.png")
+
+user_image = Label(frame, image = "")
+user_image.place(x=80,y = 100)
+
+comp_image = Label(frame, image = "")
+comp_image.place(x=600,y = 100)
+
+def Rock():
+	user = "Rock"
+	computer = random.choice(["Rock","Scissors","Paper"])
+	user_image.config(image=rock_png)
+	if user == computer:
+		label4.config(text="draw")
+		comp_image.config(image=rock_png)
+	elif computer == "Paper":
+		label4.config(text="computer win!")
+		comp_image.config(image=scissors_png)
+	else:
+		label4.config(text="U win!")
+		comp_image.config(image=paper_png)
+
+label4 = Label(frame, text="",font="normal 20",width = 15, borderwidth = 2,relief = "solid")
+label4.place(x=275,y=250)
+
+
+b1 = Button(frame, text="Rock", font= "10", width =20, command=Rock)
+b1.place(x=100,y=300)
+
+b2 = Button(frame, text="scissors", font= "10", width =20)
+b2.place(x=300,y=300)
+
+b3 = Button(frame, text="paper", font= "10", width =20)
+b3.place(x=500,y=300)
 
 window.mainloop()
